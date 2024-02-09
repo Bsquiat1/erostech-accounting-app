5980e39d62d02b4f3ccd48d2427c1faeafe04a6f
japhethnyaranga <nyaranga4@gmail.com>
2024-02-09 11:22:17 +0300

    extra files

--- src/components/invoices/Invoices.jsx
+++ src/components/invoices/Invoices.jsx
@@ -110,18 +110,22 @@ function Invoices() {
                 <button 
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center mr-10"
                   onClick={() => { }}
-                ><Link to="/final-invoice">Generate Invoice</Link></button>
+                >
+                  <Link to="/final-invoice">Generate Invoice</Link>
+                </button>
               </div>
               <div className="col">
                 <button 
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center mr-10"
-                  onClick={() => { Link="/profoma-invoice" }}
-                ><Link to="/profoma-invoice">Generate Proforma Invoice</Link></button>
+                >
+                  <Link to="/profoma-invoice">Generate Proforma Invoice</Link>
+                </button>
               </div>
             </div>
           </div>
         </Modal.Body>
       </Modal>
+
     </div>
   );
 }
